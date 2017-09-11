6df6f318193fc2bfe7ff7e0747d3b0b26ef2c2b9
matt <matt@protectamerica.com>
2017-09-11 11:16:36 -0500

    allow user to pick between articles

--- detector/urls.py
+++ detector/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 from . import views
-
+app_name = 'detector'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<article_id>[0-9]+)/$', views.detail, name='detail' )
